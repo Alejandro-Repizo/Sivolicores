ebc9eb04b7ba372d322596c0cc6c51039ee0b14a
Alejandro Repizo <alejo.oafr2001@gmail.com>
2020-10-25 20:07:02 -0500

    index responsive

--- Sitio-web/js/main.js
+++ Sitio-web/js/main.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(window).scroll(function(){
+    //Parallax
+    $(window).scroll(function () {
         var barra = $(window).scrollTop();
         var posicion = barra * 0.01;
 
@@ -8,35 +9,65 @@ $(document).ready(function(){
             'background-position': '0 -' + posicion + 'px'
         });
     });
-    
+
+
+    //Menu Dashboard
     var altura = $('.menu').offset().top;
 
-    $(window).on('scroll', function(){
-        if($(window).scrollTop() > altura){
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > altura) {
             $('.menu').addClass('menu-oculto'),
-            $('.menu2').addClass('menu-activo'),
-            $('.menu3').addClass('menu-fixed');
+                $('.menu2').addClass('menu-activo'),
+                $('.menu3').addClass('menu-fixed');
         } else {
             $('.menu').removeClass('menu-oculto'),
-            $('.menu2').removeClass('menu-activo'),
-            $('.menu3').removeClass('menu-fixed');
+                $('.menu2').removeClass('menu-activo'),
+                $('.menu3').removeClass('menu-fixed');
         }
     });
+
+
+    $('.menuMobileWrapper li:has(ul)').click(function(e){
+        e.preventDefault();
     
-    
+        if ($(this).hasClass('activado')){
+            $(this).removeClass('activado');
+            $(this).children('ul').slideUp();
+        } else{
+            $('.menuMobileWrapper  li ul').slideUp();
+            $('.menuMobileWrapper  li').removeClass('activado');
+            $(this).addClass('activado');
+            $(this).children('ul').slideDown();
+        }
+    });
+   
+    $('.menuMobileWrapper li ul li a').click(function(){
+        window.location.href = $(this).attr("href");
+    });
+
+    $("#btn-burger").click(function(e) { 
+        e.preventDefault();
+        if($('#menuMobileWrapper').hasClass('contenedor-menu-active')){
+            $('#menuMobileWrapper').removeClass('contenedor-menu-active');  
+            $('#menuMobileWrapper').children('ul').slideUp();
+        } else {
+            $('#menuMobileWrapper').addClass('contenedor-menu-active');    
+            $('#menuMobileWrapper').children('ul').slideDown();
+        }
+    });
 
 });
 
 
+//Menu de las pagiunas
 var altura2 = $('.menu3').offset().top;
 
-    $(window).on('scroll', function(){
-        if($(window).scrollTop() > altura2){
-            $('.menu3').addClass('menu-oculto2'),
+$(window).on('scroll', function () {
+    if ($(window).scrollTop() > altura2) {
+        $('.menu3').addClass('menu-oculto2'),
             $('.menu4').addClass('menu-fixed');
-        } else {
-            $('.menu3').removeClass('menu-oculto2'),
+    } else {
+        $('.menu3').removeClass('menu-oculto2'),
             $('.menu4').removeClass('menu-fixed');
-        }
-    });
-    
+    }
+});
